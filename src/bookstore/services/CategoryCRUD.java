f1f2a63d1bbb7fae17ecf060d503cc583aed59f7
khoul <khoul@DESKTOP-HK705N2>
2020-12-06 11:24:46 +0100

    added CategoryCrud

---
+++ src/bookstore/services/CategoryCRUD.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.services;
+
+import bookstore.MyConnection;
+import bookstore.entities.Book;
+import bookstore.entities.Category;
+import bookstore.temp.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author khoul
+ */
+public class CategoryCRUD {
+    Connection cnx = MyConnection.getInstance().getCnx();
+
+     
+     //ADD_Book
+     public void addBook(Category cat){
+        try {
+            String request = "INSERT INTO category(id,name,description) VALUES"
+                    +"('"+cat.getId()+"'"
+                    +",'"+cat.getName()+"'"
+                    +",'"+cat.getDescription()+"')";
+            
+            Statement st = cnx.createStatement();
+            st.executeUpdate(request);
+            System.out.println("Category added !");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());}
+    }
+    public List<Category> listCategories(){
+        List<Category> myList = new ArrayList<Category>();
+        try {
+            String request = "select * from category";
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(request);
+            while(rs.next()){
+            Category cat = new Category();
+            cat.setId(rs.getInt(1));
+            cat.setName(rs.getString(2));
+            cat.setDescription(rs.getString(3));
+                     
+            myList.add(cat);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+        //delete
+    public void deleteCategory(Category cat){
+        try {
+            String request = "DELETE FROM category where id =?";
+            PreparedStatement pst = cnx.prepareStatement(request);
+            pst.setInt(1, cat.getId());
+           
+            pst.executeUpdate();
+            System.out.println("Category deleted!");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());        }
+    }
+    
+        public void UpdateCategory(Category cat,int id){
+        try {
+            String request="UPDATE category set name=?, description=? WHERE id=?";
+            PreparedStatement pst = cnx.prepareStatement(request);
+            pst.setString(1, cat.getName());
+            pst.setString(2, cat.getDescription());
+            pst.setInt(3, id);
+            pst.executeUpdate();
+            System.out.println("Category updated");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());        }
+    }
+    
+    
+}
