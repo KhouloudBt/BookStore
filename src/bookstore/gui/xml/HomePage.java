8c966c116b8be075ef0bb7156c51b321aa79f421
emnak16 <emnakrbi1814@gmail.com>
2020-12-11 20:39:38 +0100

    jfoenix added

---
+++ src/bookstore/gui/xml/HomePage.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.gui.xml;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Emna
+ */
+public class HomePage extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root1 = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+            Scene scene = new Scene(root1);
+            primaryStage.setTitle("home");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
