834f4ebfb2afd56f0abab96d0bf3015e8ccd954b
emnak16 <emnakrbi1814@gmail.com>
2020-12-11 01:47:00 +0100

    register gui has been added!

---
+++ src/bookstore/gui/xml/Inscription.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.gui.xml;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Emna
+ */
+public class Inscription extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root1 = FXMLLoader.load(getClass().getResource("Inscription.fxml"));
+            Scene scene = new Scene(root1);
+            primaryStage.setTitle("heloo!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
