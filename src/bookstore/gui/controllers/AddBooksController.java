741eebb680bd0608a5a03a96ff9a2ed682902995
khoul <khoul@DESKTOP-HK705N2>
2020-12-11 01:57:14 +0100

    cupdated add books

--- src/bookstore/gui/controllers/AddBooksController.java
+++ src/bookstore/gui/controllers/AddBooksController.java
@@ -23,7 +23,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
