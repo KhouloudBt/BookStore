237b96ff797fa7857e502817261d957ad857fb0a
khoul <khoul@DESKTOP-HK705N2>
2020-12-12 19:08:13 +0100

    Added item

---
+++ src/bookstore/gui/controllers/ItemController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.gui.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author khoul
+ */
+public class ItemController implements Initializable {
+
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label priceLable;
+    @FXML
+    private ImageView img;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void click(MouseEvent event) {
+    }
+    
+}
